b622395f8009570033fda4eff66c31d2661ab083
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2023-08-31 23:42:48 +0530

    Colors Section

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -3,7 +3,35 @@ import "./Colors.css";
 
 function Colors() {
   return (
-    <div>Colors</div>
+    <div>
+
+      <h2 className='sidebar-title'>Colors</h2>
+      
+      <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>Green
+      </label>
+      <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>White
+      </label>
+    </div>
   )
 }
 
