27c5319f8228049af15305c889f6f026031c903d
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2023-09-02 22:33:42 +0530

    Buttons Componenet Refactor

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,20 +1,21 @@
+import Button from "../components/Button";
 import "./Recommended.css";
 
-import React from 'react'
+const Recommended = ({ handleClick }) => {
+  return (
+    <>
+      <div>
+        <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-flex">
+          <Button onClickHandler={handleClick} value="" title="All Products" />
+          <Button onClickHandler={handleClick} value="Nike" title="Nike" />
+          <Button onClickHandler={handleClick} value="Adidas" title="Adidas" />
+          <Button onClickHandler={handleClick} value="Puma" title="Puma" />
+          <Button onClickHandler={handleClick} value="Vans" title="Vans" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-function Recommended() {
-  return <>
-  <div>
-    <h2 className="recommended-title">Recommended</h2>
-    <div className="recommended-flex">
-      <button className="btns">All Products</button>
-      <button className="btns">Nike</button>
-      <button className="btns">Adidas</button>
-      <button className="btns">Puma</button>
-      <button className="btns">Vans</button>
-    </div>
-  </div>
-</>
-}
-
-export default Recommended
+export default Recommended;
