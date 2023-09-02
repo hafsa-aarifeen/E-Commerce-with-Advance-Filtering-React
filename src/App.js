85719d5aabc94af0871b1d6732a7e9bcbd0f5d9d
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2023-09-02 21:14:34 +0530

    Input Filter Functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
+//Database
+import products from "./db/data";
+
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
   return <>
     <Sidebar />
     <Navigation />
