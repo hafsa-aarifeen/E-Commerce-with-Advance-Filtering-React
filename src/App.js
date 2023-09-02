416d56e427b1eca67c2b4b8095bdf8da8de54107
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2023-09-02 21:30:55 +0530

    Buttons Filter Functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Sidebar from "./Sidebar/Sidebar";
 
 //Database
 import products from "./db/data";
+import Card from "./components/Card";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -25,6 +26,48 @@ function App() {
   const handleChange = (event) => {
     setSelectedCategory(event.target.value);
   };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
 
   return <>
     <Sidebar />
